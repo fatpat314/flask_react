7772121d9e95d0d1ac6e474b9730bfa06ff3dbe2
Patrick <w.patrick.kelly@gmail.com>
2021-11-19 14:35:09 -0800

    creating forms

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
+from forms import Todo
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'password'
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
@@ -13,7 +16,15 @@ def hello_world():
 @app.route('/name/<string:first_name>')
 def name(first_name):
     return f'{first_name}'
-    
+
+@app.route('/todo', methods=['GET', 'POST'])
+def todo():
+    todo_form = Todo()
+    if todo_form.validate_on_submit():
+        print(todo_form.content.data)
+        return redirect('/')
+    return render_template('todo.html', form=todo_form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
